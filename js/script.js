d3608a37cea1bbcc10c03e9a615fcac47dca8777
Slava <example@gmail.com>
2021-11-30 02:01:06 +0300

    Сделал 2 задание, все найс

--- js/script.js
+++ js/script.js
@@ -1,22 +1,47 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
-
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 const personalMovieDB = {
 
-    count : numberOfFilms,
-    movies : {},
-    actors : {},
-    genders : [],
-    privat : false,
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genders: [],
+    privat: false,
 };
 
-const answer11 = prompt('Один их последних просмотренных фильмов?','');
-const answer12 = prompt('На сколько оцените его?','');
-const answer21 = prompt('Один их последних просмотренных фильмов?','');
-const answer22 = prompt('На сколько оцените его?','');
+for (let i = 0; i < 2; i++) {
+    // let flag = false;
+    // let answer11, answer12;
+    // while (!flag) {
+    //     answer11 = prompt('Один их последних просмотренных фильмов?', '');
+    //     answer12 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[answer11] = answer12;
-personalMovieDB.movies[answer21] = answer22;
+    //     if ((answer11.length <= 50) && (answer11 != '') && (answer11 != null) && (answer12.length <= 50) && (answer12 != '') && (answer12 != null)) {
+    //         flag = true;
+    //     }
+    // }
+    // personalMovieDB.movies[answer11] = answer12;
+
+    let answer11 = prompt('Один их последних просмотренных фильмов?', ''),
+        answer12 = prompt('На сколько оцените его?', '');
+
+    while (!((answer11.length <= 50) && (answer11 != '') && (answer11 != null) && (answer12.length <= 50) && (answer12 != '') && (answer12 != null))) {
+        answer11 = prompt('Один их последних просмотренных фильмов?', '');
+        answer12 = prompt('На сколько оцените его?', '');
+    }
+
+    personalMovieDB.movies[answer11] = answer12;
+}
+
+if (10 > personalMovieDB.count) {
+    console.log('Просмотрено слишком мало фильмов');
+} else if (personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
--- js/script1.js
+++ js/script1.js
@@ -7,13 +7,50 @@ number = 20;
 console.log(number);
 
 const obj = {
-    name : "Slava",
-    age : 22,
-    isMarried: false 
+    name: "Slava",
+    age: 22,
+    isMarried: false
 };
 
 console.log(obj.name);
 console.log(obj["name"]);
 
-let arr = ['sl.png','oran.jpg','apple.bmp', 5, {}, []];
+let arr = ['sl.png', 'oran.jpg', 'apple.bmp', 5, {},
+    []
+];
 console.log(arr[1]);
+
+//Тернарный оператор сравнения
+
+let num = 50;
+(num == 50) ? console.log("Ok!"): console.log("Error");
+
+switch (num) {
+    case 22:
+        console.log("No");
+        break;
+    case 50:
+        console.log("Ok");
+        break;
+    default:
+        console.log("Not this time");
+        break;
+}
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+do {
+    console.log(num);
+    num++;
+}
+while (num <= 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+    console.log(i);
+}
