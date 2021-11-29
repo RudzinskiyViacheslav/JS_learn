04c458fd35a886b30473760488e5da6b09a41f4f
Slava <example@gmail.com>
2021-11-29 22:19:47 +0300

    Все работает супер

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели','');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
 
 const personalMovieDB = {
 
@@ -8,15 +8,15 @@ const personalMovieDB = {
     movies : {},
     actors : {},
     genders : [],
-    privat : false
-}
+    privat : false,
+};
 
-let answer11 = prompt('Один их последних просмотренных фильмов?','');
-let answer12 = prompt('На сколько оцените его?','');
-let answer21 = prompt('Один их последних просмотренных фильмов?','');
-let answer22 = prompt('На сколько оцените его?','');
+const answer11 = prompt('Один их последних просмотренных фильмов?','');
+const answer12 = prompt('На сколько оцените его?','');
+const answer21 = prompt('Один их последних просмотренных фильмов?','');
+const answer22 = prompt('На сколько оцените его?','');
 
-personalMovieDB.monies.answer11 = answer12;
-personalMovieDB.movies.answer21 = answer22;
+personalMovieDB.movies[answer11] = answer12;
+personalMovieDB.movies[answer21] = answer22;
 
-console.log(personalMovieDB.movies);
+console.log(personalMovieDB);
